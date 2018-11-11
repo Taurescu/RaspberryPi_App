a6cce167b5491fbfaf28bc73ff971b27e9c7973e
Taurescu Alexandru <taurescu.alexandru@yahoo.ro>
2018-11-11 22:24:16 +0200

    Added Logging method, still in progress

--- Pi_Server/Pi_Server/src/Pi_Server/EntryPoint.h
+++ Pi_Server/Pi_Server/src/Pi_Server/EntryPoint.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <vector>
 
 #ifdef PIS_PLATFORM_WINDOWS
 
@@ -6,6 +7,14 @@ extern PIS::Server* PIS::CreateServer();
 
 int main(int argc, char **argv)
 {
+	PIS::Log::Init();
+
+	PIS_WARN("Initialized Log!");
+	int b = 6;
+	int* a = &b;
+	PIS_INFO("Hello! {}", (void*)a);
+	APP_ERROR("NO VAR");
+
 
 	auto app = PIS::CreateServer();
 	app->Run();
--- Pi_Server/Pi_Server/src/TCP.h
+++ Pi_Server/Pi_Server/src/TCP.h
@@ -4,6 +4,7 @@
 
 #include "Pi_Server\Server.h"
 #include "Pi_Server\Core.h"
+#include "Pi_Server\Log.h"
 
 //-----Entry Point-------------
 #include "Pi_Server\EntryPoint.h"
